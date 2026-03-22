7d240b5c9e3e12c31d6e3afe648af011d9753cc9
ochiochi0729 <ochiochi0729@gmail.com>
2026-03-22 17:09:27 +0900

    Update walk_forward_test.py

--- walk_forward_test.py
+++ walk_forward_test.py
@@ -11,7 +11,7 @@ from strategies.breakout import BreakoutScreener
 # ★ 戦略の選択
 # ==========================================
 # "PO" = パーフェクトオーダー / "CWH" = カップウィズハンドル
-STRATEGY_TYPE = "CWH" 
+STRATEGY_TYPE = "BO" 
 
 STRATEGY_MAP = {
     "PO": {"class": PerfectOrderScreener, "name": "Perfect Order"},
